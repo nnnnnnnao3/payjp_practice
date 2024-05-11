fd3b1ba298495dd9923e62fc60204dd65b0f4033
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-05-11 15:13:19 +0900

    デプロイのためのコミット

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = "sk_test_2ccea9fe659d4bbf92074579"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+end
